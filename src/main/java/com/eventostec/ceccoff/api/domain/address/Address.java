e56f969bf59952af9dc108c53c6bef8a7734aa5f
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-25 18:44:06 -0300

    feat: :sparkles: Add JPA entities to mapping tables in Java Object

---
+++ src/main/java/com/eventostec/ceccoff/api/domain/address/Address.java
@@ -0,0 +1,29 @@
+package com.eventostec.ceccoff.api.domain.address;
+
+import com.eventostec.ceccoff.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
---
+++ src/main/java/com/eventostec/ceccoff/api/domain/event/Event.java
@@ -0,0 +1,33 @@
+package com.eventostec.ceccoff.api.domain.event;
+
+import com.eventostec.ceccoff.api.domain.address.Address;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "event")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String title;
+    private String description;
+    private String imgUrl;
+    private String eventUrl;
+    private Boolean remote;
+    private Date date;
+
+    @OneToOne(mappedBy = "event", cascade = CascadeType.ALL)
+    private Address address;
+}
