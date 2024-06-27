0b8f527e99923386467a3a58242300174c176ed3
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-26 23:44:12 -0300

    feat: :sparkles: Add find filtered events JPA select

--- src/main/java/com/eventostec/ceccoff/api/repository/EventRepository.java
+++ src/main/java/com/eventostec/ceccoff/api/repository/EventRepository.java
@@ -7,12 +7,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<Event, UUID> {
-    @Query("SELECT e FROM Event e WHERE e.date >= :currentDate")
+    @Query("SELECT e FROM Event e LEFT JOIN FETCH e.address a WHERE e.date >= :currentDate")
     Page<Event> findUpcomingEvents(@Param("currentDate") LocalDateTime currentDate, Pageable pageable);
+
+    @Query("SELECT e FROM Event e LEFT JOIN e.address a " +
+            "WHERE (:title = '' OR e.title LIKE %:title% )" +
+            "AND (:city = '' OR a.city LIKE %:city% )" +
+            "AND (:uf = '' OR a.uf LIKE %:uf% )" +
+            "AND (e.date >= :startDate AND e.date <= :endDate )")
+    Page<Event> findFilteredEvents(
+            @Param("title") String title,
+            @Param("city") String city,
+            @Param("uf") String uf,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate,
+            Pageable pageable
+    );
 }
