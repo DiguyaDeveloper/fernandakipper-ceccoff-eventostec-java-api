e1cb335ef8a7f9c607f3156f77ebcf0cac057c80
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-27 00:54:17 -0300

    refactor: :recycle: Refactor coupon table to save expireIn and validFrom

--- src/main/java/com/eventostec/ceccoff/api/domain/coupon/CouponRequestDTO.java
+++ src/main/java/com/eventostec/ceccoff/api/domain/coupon/CouponRequestDTO.java
@@ -1,7 +1,12 @@
 package com.eventostec.ceccoff.api.domain.coupon;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
 public record CouponRequestDTO(
         String code,
         Integer discount,
-        Long valid) {
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate expireIn,
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate validFrom) {
 }
