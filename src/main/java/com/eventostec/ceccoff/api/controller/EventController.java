466ac0ecd39184e7e782202af5005f4333449e01
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-26 22:16:09 -0300

    feat: :sparkles: Add accept datetimeformat in request param

--- src/main/java/com/eventostec/ceccoff/api/controller/EventController.java
+++ src/main/java/com/eventostec/ceccoff/api/controller/EventController.java
@@ -5,10 +5,12 @@ import com.eventostec.ceccoff.api.domain.event.EventRequestDTO;
 import com.eventostec.ceccoff.api.domain.event.EventResponseDTO;
 import com.eventostec.ceccoff.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -29,7 +31,7 @@ public class EventController {
     public ResponseEntity<Event> create(
             @RequestParam("title") String title,
             @RequestParam(value = "description", required = false) String description,
-            @RequestParam("date") Long date,
+            @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime date,
             @RequestParam("city") String city,
             @RequestParam("state") String state,
             @RequestParam("remote") Boolean remote,
