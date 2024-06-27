227c0c7b0e601d4e0f80af93fb1bb4f546fb2f26
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-27 00:55:28 -0300

    refactor: :recycle: Refactor query to consult valid coupons of event

--- src/main/java/com/eventostec/ceccoff/api/repository/CouponRepository.java
+++ src/main/java/com/eventostec/ceccoff/api/repository/CouponRepository.java
@@ -2,14 +2,18 @@ package com.eventostec.ceccoff.api.repository;
 
 import com.eventostec.ceccoff.api.domain.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
 
-    List<Coupon> findByEventIdAndValidAfter(UUID eventId, LocalDateTime currentDate);
+    @Query("SELECT e FROM Coupon e LEFT JOIN FETCH e.event a " +
+            "WHERE e.validFrom >= :validFrom " +
+            "AND e.expireIn < :expireIn " +
+            "AND a.id = :eventId ")
+    List<Coupon> findByEventIdAndValidFromLessThanEqualAndExpireInGreaterThanEqual(UUID eventId, LocalDate validFrom, LocalDate expireIn);
+
 }
