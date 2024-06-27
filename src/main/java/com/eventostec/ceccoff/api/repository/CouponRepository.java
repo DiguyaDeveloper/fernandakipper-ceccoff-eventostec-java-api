00904688de163ca74d1c07a00331eef6e402ab2e
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-27 00:15:42 -0300

    feat: :sparkles: Add function to get coupons by event id

--- src/main/java/com/eventostec/ceccoff/api/repository/CouponRepository.java
+++ src/main/java/com/eventostec/ceccoff/api/repository/CouponRepository.java
@@ -3,7 +3,13 @@ package com.eventostec.ceccoff.api.repository;
 import com.eventostec.ceccoff.api.domain.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+
+    List<Coupon> findByEventIdAndValidAfter(UUID eventId, LocalDateTime currentDate);
 }
