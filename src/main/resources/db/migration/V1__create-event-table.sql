37aa227012f947a7ca99a5837aac59634d95e281
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-26 21:39:18 -0300

    feat: :sparkles: Update migration of create table modify size of columns

--- src/main/resources/db/migration/V1__create-event-table.sql
+++ src/main/resources/db/migration/V1__create-event-table.sql
@@ -3,8 +3,8 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE TABLE event (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    description VARCHAR(250) NOT NULL,
-    img_url VARCHAR(100) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    img_url VARCHAR(500) NOT NULL,
     event_url VARCHAR(100) NOT NULL,
     date TIMESTAMP NOT NULL,
     remote BOOLEAN NOT NULL
