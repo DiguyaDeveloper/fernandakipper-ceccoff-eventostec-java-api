12735aaf3831afeaab218d1af79b77ee10335ed3
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-26 21:40:41 -0300

    feat: :sparkles: Consume fileservice and add getupcoming events function

--- src/main/java/com/eventostec/ceccoff/api/service/EventService.java
+++ src/main/java/com/eventostec/ceccoff/api/service/EventService.java
@@ -2,19 +2,31 @@ package com.eventostec.ceccoff.api.service;
 
 import com.eventostec.ceccoff.api.domain.event.Event;
 import com.eventostec.ceccoff.api.domain.event.EventRequestDTO;
+import com.eventostec.ceccoff.api.domain.event.EventResponseDTO;
+import com.eventostec.ceccoff.api.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class EventService {
+
+    @Autowired
+    private FileService fileService;
+
+    @Autowired
+    private EventRepository repository;
 
     public Event create(EventRequestDTO request) {
         String imgUrl = null;
 
         if (request.image() != null) {
-            imgUrl = this.uploadImg(request.image());
+            imgUrl = fileService.uploadImg(request.image());
         }
 
         Event newEvent = new Event();
@@ -23,11 +35,30 @@ public class EventService {
         newEvent.setEventUrl(request.eventUrl());
         newEvent.setDate(new Date(request.date()));
         newEvent.setImgUrl(imgUrl);
+        newEvent.setRemote(request.remote());
+
+        repository.save(newEvent);
 
         return newEvent;
     }
 
-    private String uploadImg(MultipartFile file) {
-        return "";
+    public List<EventResponseDTO> getUpcomingEvents(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<Event> eventsPage = this.repository.findUpcomingEvents(new Date(), pageable);
+
+        return eventsPage.map(event -> new EventResponseDTO(
+                event.getId(),
+                event.getTitle(),
+                event.getDescription(),
+                event.getDate(),
+                event.getAddress() != null ? event.getAddress().getCity() : "",
+                event.getAddress() != null ? event.getAddress().getUf() : "",
+                event.getRemote(),
+                event.getEventUrl(),
+                event.getImgUrl())
+        ).stream().toList();
     }
+
+
 }
