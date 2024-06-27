98e378be04254c2c3f6a7c7436d3ad3c17a11dca
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-27 00:15:37 -0300

    feat: :sparkles: Add function to get coupons by event id

--- src/main/java/com/eventostec/ceccoff/api/service/CouponService.java
+++ src/main/java/com/eventostec/ceccoff/api/service/CouponService.java
@@ -12,7 +12,10 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -35,6 +38,10 @@ public class CouponService {
         coupon.setEvent(event);
 
         return couponRepository.save(coupon);
+    }
+
+    public List<Coupon> getCouponsOfEvent(UUID eventId, LocalDateTime currentDate) {
+        return couponRepository.findByEventIdAndValidAfter(eventId, currentDate);
     }
 
 }
