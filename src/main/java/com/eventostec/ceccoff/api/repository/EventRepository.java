9f657a9e3a54733ddaab52e002d8885229fb4519
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-26 21:42:10 -0300

    feat: :sparkles: Add method with JPA query to find upcoming events

--- src/main/java/com/eventostec/ceccoff/api/repository/EventRepository.java
+++ src/main/java/com/eventostec/ceccoff/api/repository/EventRepository.java
@@ -1,9 +1,17 @@
 package com.eventostec.ceccoff.api.repository;
 
 import com.eventostec.ceccoff.api.domain.event.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<Event, UUID> {
+    @Query("SELECT e FROM Event e WHERE e.date >= :currentDate")
+    Page<Event> findUpcomingEvents(@Param("currentDate") Date currentDate, Pageable pageable);
 }
